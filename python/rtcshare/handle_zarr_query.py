f6f847eb666cab5599e5b9f6ed273a9fdb895f59
Jeremy Magland <jmagland@flatironinstitute.org>
2023-04-21 10:04:46 -0400

    zarr service query

--- python/rtcshare/handle_zarr_query.py
+++ python/rtcshare/handle_zarr_query.py
@@ -1,7 +1,6 @@
 import os
 import zarr
 from typing import Tuple
-import cv2
 
 
 def handle_zarr_query(query: dict, *, dir: str) -> Tuple[dict, bytes]:
@@ -50,64 +49,3 @@ def to_slice_objects(x: list[dict]):
 
     # Return the tuple of slice objects
     return tuple(slice_objects)
-
-def get_video_info(path: str) -> dict:
-    print(f'Getting video info from {path}')
-    
-    # Open the video file
-    cap = cv2.VideoCapture(path)
-    
-    # Get the video info
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    
-    # Release the video capture object
-    cap.release()
-    
-    # Return the video info
-    return {
-        'width': width,
-        'height': height,
-        'fps': fps,
-        'frame_count': frame_count
-    }
-
-def get_frames_from_video_file(path: str, *, start_frame: int, end_frame: int, quality: int) -> bytes:
-    print(f'Getting frames from {path} ({start_frame} - {end_frame})')
-
-    # Open the video file
-    cap = cv2.VideoCapture(path)
-
-    # Validate frame range
-    if start_frame < 0 or end_frame > int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) or start_frame >= end_frame:
-        raise ValueError("Invalid frame range")
-    
-    # Set the starting frame
-    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
-    
-    # Initialize a list to hold the JPG blobs
-    jpg_blobs = []
-    
-    # Loop through the specified frames
-    for i in range(start_frame, end_frame):
-        # Read the frame
-        ret, frame = cap.read()
-        if not ret:
-            break
-        
-        # Convert the frame to JPG
-        # Encode frame to JPG with the specified quality
-        ret, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, quality])
-        if not ret:
-            raise RuntimeError("Failed to encode frame as JPG")
-        
-        # Append the JPG blob to the list
-        jpg_blobs.append(buffer)
-    
-    # Release the video capture object
-    cap.release()
-    
-    # Concatenate the JPG blobs into one binary output
-    return b''.join(jpg_blobs)
