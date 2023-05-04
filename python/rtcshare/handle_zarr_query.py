cde28240613e55f00291f78830e00f0703cff37d
Jeremy Magland <jmagland@flatironinstitute.org>
2023-05-04 10:22:54 -0400

    zarr get_array_info

--- python/rtcshare/handle_zarr_query.py
+++ python/rtcshare/handle_zarr_query.py
@@ -5,33 +5,46 @@ from typing import Tuple
 
 def handle_zarr_query(query: dict, *, dir: str) -> Tuple[dict, bytes]:
     type0 = query['type']
-    if type0 == 'get_array_chunk':
-        path = query['path']
-        if path.startswith('$dir'):
-            path = f'{dir}/{path[len("$dir"):]}'
-        name = query['name']
-        slices = query['slices']
-        slice_objects = to_slice_objects(slices)
+    if type0 == 'get_array_chunk' or type0 == 'get_array_info':
+        try:
+            path = query['path']
+            if path.startswith('$dir'):
+                path = f'{dir}/{path[len("$dir"):]}'
+            name = query['name']
 
-        if not path.startswith('rtcshare://'):
-            raise Exception(f'Invalid path: {path}')
-        relpath = path[len('rtcshare://'):]
-        if '..' in relpath: # for security
-            raise Exception(f'Invalid path: {path}')
-        fullpath = os.path.join(os.environ['RTCSHARE_DIR'], relpath)
-        if not os.path.exists(fullpath):
-            raise Exception(f'Folder does not exist: {fullpath}')
-        root_group = zarr.open(fullpath, 'r')
-        zarr_array = root_group[name]
-        zarr_array_slices = zarr_array[slice_objects]
-        numpy_array = zarr_array_slices[:]
-        result = {
-            'dtype': str(numpy_array.dtype),
-        }
-        # ensure we have c order
-        if not numpy_array.flags.c_contiguous:
-            numpy_array = numpy_array.copy(order='C')
-        return result, numpy_array.tobytes()
+            if not path.startswith('rtcshare://'):
+                raise Exception(f'Invalid path: {path}')
+            relpath = path[len('rtcshare://'):]
+            if '..' in relpath: # for security
+                raise Exception(f'Invalid path: {path}')
+            fullpath = os.path.join(os.environ['RTCSHARE_DIR'], relpath)
+            if not os.path.exists(fullpath):
+                raise Exception(f'Folder does not exist: {fullpath}')
+            root_group = zarr.open(fullpath, 'r')
+            zarr_array = root_group[name]
+            result = {
+                'success': True,
+                'dtype': str(zarr_array.dtype),
+                'shape': [int(x) for x in zarr_array.shape]
+            }
+
+            if type0 == 'get_array_info':
+                return result, b''
+            elif type0 == 'get_array_chunk':
+                slices = query['slices']
+                slice_objects = to_slice_objects(slices)
+                zarr_array_slices = zarr_array[slice_objects]
+                numpy_array = zarr_array_slices[:]
+                
+                # ensure we have c order
+                if not numpy_array.flags.c_contiguous:
+                    numpy_array = numpy_array.copy(order='C')
+                return result, numpy_array.tobytes()
+            else:
+                raise Exception(f'Unexpected zarr query type: {type0}')
+        except Exception as e:
+            print(f'Error while handling zarr query: {e}')
+            return {'success': False, 'error': str(e)}, b''
     else:
         raise Exception(f'Unknown video query type: {type0}')
 
